d6ea0cf05e64e9eace90b7cd3410e87553d7f289
Gurkan <hello@gurkz.me>
2024-02-08 08:48:58 +0000

    fix

--- src/pages/projects/most-beautiful-pokemon/image/[id].png.ts
+++ src/pages/projects/most-beautiful-pokemon/image/[id].png.ts
@@ -4,11 +4,11 @@ export async function GET({ params }: { params: { id: string } }) {
   if (typeof mon !== "number")
       return new Response("Not found", { status: 404 });
 
-  const response = await fetch(
+  const response: Response = await fetch(
       `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${mon}.png`
   );
 
-  const buffer = Buffer.from(response.arrayBuffer);
+  const buffer = Buffer.from(await response.arrayBuffer());
 
   const newRes = new Response(buffer, {
     headers: { "Content-Type": "image/png", "Cache-Control": "max-age=31536000, immutable" }
