5a27ba1b98a4a47500a162cdd3cc162577b2daed
Gurkan <hello@gurkz.me>
2024-02-12 12:42:25 +0100

    dark mode

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,27 @@
 // @refresh reload
 import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start";
-import { Suspense } from "solid-js";
+import { FileRoutes, ServerContext } from "@solidjs/start";
+import { Suspense, useContext } from "solid-js";
+import { ColorModeProvider, ColorModeScript, cookieStorageManagerSSR } from "@kobalte/core";
 import "./app.css";
 
 export default function App() {
+  const event = useContext(ServerContext);
+
+  const storageManager = cookieStorageManagerSSR(
+    isServer ? event?.request.headers.get("cookie") ?? "" : document.cookie
+  );
+
   return (
     <Router
       root={(props) => (
         <>
-          <Suspense>{props.children}</Suspense>
+          <ColorModeScript storageType={storageManager.type} />
+          <Suspense>
+            <ColorModeProvider storageManager={storageManager}>
+               {props.children}
+            </ColorModeProvider>
+          </Suspense>
         </>
       )}
     >
