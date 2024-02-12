16922cfea57e455b0862ad3738f8a2cd5a0bfc38
Gurkan <hello@gurkz.me>
2024-02-12 16:52:04 +0100

    add dark/light mode toggle

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,34 @@
-// @refresh reload
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start";
 import { Suspense } from "solid-js";
 import "./app.css";
+import { getRequestEvent, isServer } from "solid-js/web";
+import {
+  ColorModeProvider,
+  ColorModeScript,
+  cookieStorageManagerSSR,
+} from "@kobalte/core";
+import { ModeToggle } from "./components/ui/theme-toggle";
 
 export default function App() {
+  const event = getRequestEvent();
+
+  const storageManager = cookieStorageManagerSSR(
+    isServer ? event?.request.headers.get("cookie") ?? "" : document.cookie
+  );
+
   return (
     <Router
       root={(props) => (
         <>
           <ColorModeScript storageType={storageManager.type} />
           <Suspense>
-            <ColorModeProvider storageManager={storageManager}>
-               {props.children}
-            </ColorModeProvider>
+            <div class="p-3">
+              <ColorModeProvider storageManager={storageManager}>
+                <ModeToggle />
+                {props.children}
+              </ColorModeProvider>
+            </div>
           </Suspense>
         </>
       )}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,15 @@
-import { A } from "@solidjs/router";
 import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
 
-export default function Home() {
+function Home() {
   return (
-    <main class="mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        hello! 👋
-      </h1>
+    <main class="mx-auto p-4">
       <Alert>
         <AlertTitle>warning</AlertTitle>
-        <AlertDescription>
-           this website is work in progress
-        </AlertDescription>
+        <AlertDescription>this website is work in progress</AlertDescription>
       </Alert>
+      <h1 class="max-6-xs text-6xl pt-4">hello! 👋</h1>
     </main>
   );
 }
+
+export default Home;
