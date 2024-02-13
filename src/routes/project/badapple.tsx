6d9d7639785497069aa4805ad2d115d102bdabcd
Gurkan <hello@gurkz.me>
2024-02-13 10:37:22 +0100

    add the video tag

--- src/routes/project/badapple.tsx
+++ src/routes/project/badapple.tsx
@@ -1,3 +1,10 @@
 export default function BadApple() {
-    return <h1 class="text-4xl pt-2">wip</h1>
+    return (
+        <>
+            <h1 class="text-4xl pt-2">bad apple in HTML</h1>
+            <video controls width="160" height="120" playsinline>
+                <source src="/badapple.mp4" type="video/mp4" />
+            </video>
+        </>
+    )
 }
