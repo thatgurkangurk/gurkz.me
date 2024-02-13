eb7a8894ecbfb7309fa90d0f4265be06da61ecf0
Gurkan <hello@gurkz.me>
2024-02-13 10:52:29 +0100

    fix video playing?

--- src/routes/project/badapple.tsx
+++ src/routes/project/badapple.tsx
@@ -1,9 +1,11 @@
+import badappleVideo from "~/badapple.mp4";
+
 export default function BadApple() {
     return (
         <>
             <h1 class="text-4xl pt-2">bad apple in HTML</h1>
             <video controls width="160" height="120" playsinline>
-                <source src="/badapple.mp4" type="video/mp4" />
+                <source src={badappleVideo} type="video/mp4" />
             </video>
         </>
     )
