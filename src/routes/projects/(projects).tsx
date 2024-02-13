a401f785d47e715c2bfd771aa3bcadde1cd1d8a1
Gurkan <hello@gurkz.me>
2024-02-13 10:19:33 +0100

    move project

--- src/routes/projects/(projects).tsx
+++ src/routes/projects/(projects).tsx
@@ -4,7 +4,7 @@ export default function Projects() {
     return (
         <>
           <h1 class="text-4xl pt-2">projects</h1>
-          <A href="/projects/badapple">bad apple in HTML</A>
+          <A href="/project/badapple">bad apple in HTML</A>
         </>
     )
 }
