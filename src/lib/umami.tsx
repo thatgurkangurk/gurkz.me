4cb475b94ed812bd8c5cac77251b9e8e7ed53125
Gurkan <hello@gurkz.me>
2024-02-14 10:24:23 +0000

    create the umami context

--- src/lib/umami.tsx
+++ src/lib/umami.tsx
@@ -1,4 +1,21 @@
 import { isServer } from "solid-js/web";
+import { createContext, useContext, type ParentComponent } from "solid-js";
+
+const UmamiContext = createContext();
+
+const UmamiProvider: ParentComponent = (props) => {
+    if (isServer) return;
+
+    return (
+        <UmamiContext.Provider value={window.umami!}>
+            {props.children}
+        </UmamiContext.Provider>
+    )
+}
+
+function useUmami() {
+    return useContext(UmamiContext);
+}
 
 function trackEvent(ev: string) {
     if (isServer) return;
@@ -12,4 +29,4 @@ function trackEvent(ev: string) {
     }
 }
 
-export { trackEvent }
+export { useUmami, trackEvent, UmamiContext, UmamiProvider }
