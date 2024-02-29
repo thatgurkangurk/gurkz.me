5bd717ae32a27a049f5c1880af05cd58f671fd24
Gurkan <hello@gurkz.me>
2024-02-29 15:51:18 +0100

    fix the "oops, something went wrong" error that could happen when you went to the project

--- src/components/pokemon/PokemonCards.tsx
+++ src/components/pokemon/PokemonCards.tsx
@@ -1,63 +1,39 @@
-import { Match, Show, Switch, VoidComponent, onMount } from "solid-js";
-import { trpc } from "~/lib/api";
+import { Match, Show, Switch, VoidComponent } from "solid-js";
 import { Button } from "../ui/button";
 import { TbLoader } from "solid-icons/tb";
 
-const PokemonCards: VoidComponent = () => {
-  const pokemon = trpc.pokemon.getPokemonPair.useQuery();
-
-  onMount(() => {
-    pokemon.refetch();
-  });
-
+const PokemonCards: VoidComponent<{ data: number[], isRefetching: boolean, refetch: () => void }> = (props) => {
   return (
     <>
-      <Switch>
-        <Match when={pokemon.isPending}>
-          <p>Loading...</p>
-        </Match>
-        <Match when={pokemon.isError}>
-          <p>Error: {pokemon.error!.message}</p>
-        </Match>
-        <Match when={pokemon.isSuccess}>
-          {/* <div class="w-full flex flex-row justify-center gap-4">
-            <PokemonCard id={pokemon.data![0]!} isLoading={false} />
-            <PokemonCard id={pokemon.data![1]!} isLoading={false} />
+      <div class="w-full flex justify-center gap-4">
+        <div class="flex-col">
+          <div class="flex-row justify-center flex gap-14">
+            <PokemonCard
+              id={props.data[0]}
+              isLoading={props.isRefetching}
+            />
+            <PokemonCard
+              id={props.data[1]}
+              isLoading={props.isRefetching}
+            />
           </div>
-
-          <Button class="gap-2">these are boring, get me new ones</Button> */}
-
-          <div class="w-full flex justify-center gap-4">
-            <div class="flex-col">
-              <div class="flex-row justify-center flex gap-14">
-                <PokemonCard
-                  id={pokemon.data![0]!}
-                  isLoading={pokemon.isRefetching}
-                />
-                <PokemonCard
-                  id={pokemon.data![1]!}
-                  isLoading={pokemon.isRefetching}
-                />
-              </div>
-              <div class="pt-2">
-                <Button
-                  disabled={pokemon.isRefetching}
-                  onClick={() => {
-                    pokemon.refetch();
-                  }}
-                  variant={"secondary"}
-                  class="gap-2"
-                >
-                  <Show when={pokemon.isRefetching}>
-                    <TbLoader class="mr-2 h-4 w-4 animate-spin" />
-                  </Show>
-                  these are boring, get me new ones
-                </Button>
-              </div>
-            </div>
+          <div class="pt-2">
+            <Button
+              disabled={props.isRefetching}
+              onClick={() => {
+                props.refetch();
+              }}
+              variant={"secondary"}
+              class="gap-2"
+            >
+              <Show when={props.isRefetching}>
+                <TbLoader class="mr-2 h-4 w-4 animate-spin" />
+              </Show>
+              these are boring, get me new ones
+            </Button>
           </div>
-        </Match>
-      </Switch>
+        </div>
+      </div>
     </>
   );
 };
@@ -79,4 +55,4 @@ const PokemonCard: VoidComponent<{ id: number; isLoading: boolean }> = (
   );
 };
 
-export { PokemonCards };
+export default PokemonCards;
--- src/routes/project/most-beautiful-pokemon.tsx
+++ src/routes/project/most-beautiful-pokemon.tsx
@@ -1,12 +1,30 @@
-import { PokemonCards } from "~/components/pokemon/PokemonCards";
+import { Match, Suspense, Switch, lazy, onMount } from "solid-js";
+import { trpc } from "~/lib/api";
+
+const PokemonCards = lazy(() => import("~/components/pokemon/PokemonCards"));
 
 export default function PokemonVoteProject() {
+  const query = trpc.pokemon.getPokemonPair.useQuery();
+  onMount(() => {
+    query.refetch();
+  });
+
   return (
     <>
       <h1>most beautiful pokémon</h1>
       <p>wip, but here are two randomly generated pokémon, just for you!</p>
 
-      <PokemonCards />
+      <Suspense fallback={<p>loading</p>}>
+        <Switch>
+          <Match when={query.isError}>
+            <p>Error: {query.error!.message}</p>
+          </Match>
+          <Match when={query.isSuccess}>
+
+            <PokemonCards data={query.data!} isRefetching={query.isRefetching} refetch={() => query.refetch()} />
+          </Match>
+        </Switch>
+      </Suspense>
     </>
   );
 }
