75bd368ba52eac79a3e8b8eafbc9d69d0d9de11e
Gurkan <hello@gurkz.me>
2024-03-03 20:45:36 +0100

    font + base layout

--- astro.config.ts
+++ astro.config.ts
@@ -5,9 +5,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
-  integrations: [tailwind()]
+	output: "server",
+	adapter: node({
+		mode: "standalone",
+	}),
+	integrations: [tailwind()],
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,18 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				geist: "Geist Mono",
+			},
+			colors: {
+				themeColor: "#5EBFA8",
+				themeBlack: "#121212",
+				themeGray: "#3d3d3d",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
