5bee84af859a459dc47fa2706965da2134e4e2ac
Gurkan <hello@gurkz.me>
2024-03-03 18:05:29 +0100

    lint + prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+/** @type {import("prettier").Config} */
+export default {
+    plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+    printWidth: 120,
+    tabWidth: 4,
+    trailingComma: "all",
+    useTabs: true
+}
