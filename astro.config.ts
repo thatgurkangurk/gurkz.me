e98407ac70b19cdb7c18aeeb702c7d3195e26da5
Gurkan <hello@gurkz.me>
2024-03-04 17:23:30 +0100

    working webhook destroyer

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,5 @@ export default defineConfig({
   adapter: node({
     mode: "standalone"
   }),
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), solidJs()]
 });
