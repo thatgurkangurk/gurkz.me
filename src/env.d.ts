ced9885efb0204b44bca7b191ba287f4b7252397
Gurkan <hello@gurkz.me>
2024-03-06 18:17:35 +0100

    use @astroutils/analytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+	readonly ANALYTICS_HOST: string;
+	readonly WEBSITE_ID: string;
+}
