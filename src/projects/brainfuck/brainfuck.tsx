f54ea6552cf1f2c4ac3baff582bacd9470851c26
Gurkan <hello@gurkz.me>
2024-03-26 12:41:23 +0100

    basic form

--- src/projects/brainfuck/brainfuck.tsx
+++ src/projects/brainfuck/brainfuck.tsx
@@ -1,7 +1,33 @@
+import { createSignal } from "solid-js";
+import { Input } from "~/components/ui/input";
+import type { JSX } from "solid-js";
+
+const [output, setOutput] = createSignal("none");
+
+const handleSubmit: JSX.EventHandler<HTMLFormEvent, SubmitEvent> = (e) => {
+    e.preventDefault();
+    setOutput("this interpreter is not done, this output is a placeholder.");
+}
+
 export function Brainfuck() {
     return (
         <>
-           <h1>coming soon</h1>
+            <span>output: {output()} </span>
+           <form onSubmit={handleSubmit}>
+				<Input
+                    id="program"
+					placeholder="the brainfuck program to execute"
+					class={`w-[90%]`}
+					style={{ "font-family": "inherit" }}
+					required
+				/>
+                <button
+					class="mt-2 w-fit rounded-md bg-red-500 px-4 py-2 text-[1.2rem]"
+					style={{ "font-family": "inherit" }}
+				>
+					run
+				</button>
+           </form>
         </>
     )
 }
