88e8662cad4194f84ad3c0576b3ad783ca0ee9a6
Gurkan <hello@gurkz.me>
2024-03-28 10:29:09 +0100

    fix?

--- src/projects/brainfuck/brainfuck.ts
+++ src/projects/brainfuck/brainfuck.ts
@@ -3,7 +3,7 @@ import { createSignal } from "solid-js";
 function useBrainfuck() {
     const [output, setOutput] = createSignal("none");
 
-    function interpret(program: string) {
+    const interpret = (program: string) => {
         console.log(program);
         setOutput("this interpreter is not done, this output is just a placeholder");
     }
