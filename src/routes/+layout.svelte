c56f05243a047c4d5d66c412b1f68bdbab727ad3
Gurkan <hello@gurkz.me>
2024-05-02 17:48:57 +0200

    shadcn-svelte setup + tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import "../app.css";
+	import { ModeWatcher } from "mode-watcher";
+</script>
+
+<ModeWatcher />
+
+<main class="p-2">
+	<slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,1 @@
-<h1>hello</h1>
+<h2 class="text-3xl">hello!</h2>
