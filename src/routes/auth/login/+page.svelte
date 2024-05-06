78c6ac05a15841f47a1ba37d645e6c6394ddef21
Gurkan <hello@gurkz.me>
2024-05-06 15:21:20 +0200

    better auth screen

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,3 +1,43 @@
+<script lang="ts">
+	import { getUser } from "$lib/auth/store";
+	import { Button } from "$lib/components/ui/button/index.js";
+	import * as Card from "$lib/components/ui/card/index.js";
+
+	let user = getUser();
+</script>
+
 <h2 class="text-3xl">sign in</h2>
-<p>this page is work in progress</p>
-<a href="/auth/login/discord">sign in with discord</a>
+
+{#if $user}
+	<Card.Root class="mx-auto max-w-sm">
+		<Card.Header>
+			<Card.Title class="text-xl">You are already signed in.</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			<Button href="/">Go Home</Button>
+		</Card.Content>
+	</Card.Root>
+{:else}
+	<Card.Root class="mx-auto max-w-sm">
+		<Card.Header>
+			<Card.Title class="text-xl">Welcome to gurkz.me!</Card.Title>
+			<Card.Description>Choose how you want to sign in below:</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<Button
+				variant="outline"
+				class="w-full bg-[#5865F2] hover:bg-[#0d1aa8]"
+				href="/auth/login/discord"
+			>
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" viewBox="0 0 127.14 96.36"
+					><path
+						fill="#fff"
+						d="M107.7,8.07A105.15,105.15,0,0,0,81.47,0a72.06,72.06,0,0,0-3.36,6.83A97.68,97.68,0,0,0,49,6.83,72.37,72.37,0,0,0,45.64,0,105.89,105.89,0,0,0,19.39,8.09C2.79,32.65-1.71,56.6.54,80.21h0A105.73,105.73,0,0,0,32.71,96.36,77.7,77.7,0,0,0,39.6,85.25a68.42,68.42,0,0,1-10.85-5.18c.91-.66,1.8-1.34,2.66-2a75.57,75.57,0,0,0,64.32,0c.87.71,1.76,1.39,2.66,2a68.68,68.68,0,0,1-10.87,5.19,77,77,0,0,0,6.89,11.1A105.25,105.25,0,0,0,126.6,80.22h0C129.24,52.84,122.09,29.11,107.7,8.07ZM42.45,65.69C36.18,65.69,31,60,31,53s5-12.74,11.43-12.74S54,46,53.89,53,48.84,65.69,42.45,65.69Zm42.24,0C78.41,65.69,73.25,60,73.25,53s5-12.74,11.44-12.74S96.23,46,96.12,53,91.08,65.69,84.69,65.69Z"
+					/></svg
+				>
+
+				Sign in with Discord
+			</Button>
+		</Card.Content>
+	</Card.Root>
+{/if}
