60b728a06ba7d75fffd71a03c6e99cf13250d409
Gurkan <hello@gurkz.me>
2024-06-20 16:02:37 +0200

    move want you gone project

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -10,6 +10,11 @@ const projects: Project[] = [
     description: "a simple tool to destroy discord webhooks",
     slug: "webhook-destroyer",
   },
+  {
+    title: "want you gone",
+    description: "portal 2's ending remade in HTML, CSS and JS",
+    slug: "want-you-gone",
+  },
 ];
 
 function getProjectBySlug(slug: string) {
