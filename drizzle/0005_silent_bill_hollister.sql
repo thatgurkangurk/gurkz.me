1d3fdf500a25df48a45622e07d507e5c28a8a65f
Gurkan <hello@gurkz.me>
2024-07-29 16:41:10 +0200

    remove everything

--- drizzle/0005_silent_bill_hollister.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "user" ALTER COLUMN "discord_id" SET DATA TYPE varchar;
--- src/lib/auth/clients.ts
+++
@@ -1,8 +0,0 @@
-import { Discord } from "arctic";
-import { getSecret } from "astro:env/server";
-
-export const discord = new Discord(
-  getSecret("DISCORD_CLIENT_ID"),
-  getSecret("DISCORD_CLIENT_SECRET"),
-  `${getSecret("SITE_URL")}/auth/discord/callback`
-);
