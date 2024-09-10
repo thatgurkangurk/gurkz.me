196cf4a338cd450123badfb9d6faa759b87a89f6
Gurkan <hello@gurkz.me>
2024-09-10 14:38:54 +0200

    add tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import "../app.css";
+	import type { Snippet } from "svelte";
+
+	type Props = {
+		children: Snippet<[]>;
+	};
+
+	let { children }: Props = $props();
+</script>
+
+{@render children()}
