19971b1f930dbd43817377f608d89d910ca611f1
Gurkan <hello@gurkz.me>
2024-11-14 21:36:20 +0100

    tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,9 @@
+import "~/lib/styles.css";
+
 export default function App() {
-  
   return (
     <main>
-      <h1>hello, world!</h1>
+      <h1 class="text-3xl">hello, world!</h1>
     </main>
   );
 }
