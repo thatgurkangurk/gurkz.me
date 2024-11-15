6737174603bbdaa11194c63037a0b2e4480506c7
Gurkan <hello@gurkz.me>
2024-11-15 16:20:30 +0100

    remove unused import

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,6 @@
-import "dotenv/config";
 import * as schema from "./schema";
 import { drizzle } from "drizzle-orm/node-postgres";
 
 export const db = drizzle(process.env.DATABASE_URL!, {
-    schema: schema
+  schema: schema,
 });
