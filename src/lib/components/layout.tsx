1c6acf714b9c25a4cbd5af7bdc4c4fb8f78c5eb8
Gurkan <hello@gurkz.me>
2024-11-15 11:59:19 +0100

    add a warning when you are on a preview deployment

--- src/lib/components/layout.tsx
+++ src/lib/components/layout.tsx
@@ -1,11 +1,16 @@
 import { MetaProvider, Title } from "@solidjs/meta";
-import type { ParentProps } from "solid-js";
+import { Show, type ParentProps } from "solid-js";
 import { Nav } from "./nav";
 
 export function Layout(props: ParentProps) {
   return (
     <MetaProvider>
       <Title>gurkan's website</Title>
+      <Show when={process.env.IS_PREVIEW === "1"}>
+        <div class="w-full text-white bg-orange-500">
+          <p>this is a preview deployment - go to the main deployment <a class="underline underline-offset-4" href="https://www.gurkz.me/">here</a></p>
+        </div>
+      </Show>
       <nav class="flex flex-row gap-2">
         <Nav links={[
           {
