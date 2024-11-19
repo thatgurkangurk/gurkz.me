a86f5f80f16871e287b1d726e89e4c007660b5a8
Gurkan <hello@gurkz.me>
2024-11-19 12:32:35 +0100

    remove unused imports

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,4 @@
-import { useAuth } from "@solid-mediakit/auth/client";
-import { Show } from "solid-js";
-
 export default function Home() {
-  const auth = useAuth();
   return (
     <div>
       <p>hello, world</p>
