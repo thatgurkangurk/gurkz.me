d536117ba5d1b35a82540f3053b52104402fd94c
Gurkan <hello@gurkz.me>
2024-11-20 09:37:36 +0100

    add auth status in nav

--- src/lib/components/nav.tsx
+++ src/lib/components/nav.tsx
@@ -59,22 +59,15 @@ export function Nav(props: { links: Link[] }) {
           </nav>
         </SheetContent>
       </Sheet>
+      <div class="ml-auto flex-1 sm:flex-initial" />
+      <div class="flex flex-row items-center break-normal gap-2">
+        <Show when={auth.session()} fallback={<Button onClick={() => auth.signIn("discord")} variant="link">log in</Button>}>
+          {(session) => <div class="flex gap-2">
+            <p>hello, {session().user.name}</p>
+            <Button onClick={() => auth.signOut()} variant="link">log out</Button>
+            </div>}
+        </Show>
+      </div>
     </header>
   );
 }
-
-// let auth: any;
-
-// const hi = (
-//   <Show
-//     when={auth.session()}
-//     fallback={<button onClick={() => auth.signIn("discord")}>sign in</button>}
-//   >
-//     {(session) => (
-//       <div class="flex gap-2">
-//         <p>hello, {session().user.name}</p>
-//         <button onClick={() => auth.signOut()}>sign out</button>
-//       </div>
-//     )}
-//   </Show>
-// );
