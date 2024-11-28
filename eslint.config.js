1dbe56e5d024f8641f6e57b4edad22116d5c2b6d
Gurkan <hello@gurkz.me>
2024-11-28 18:43:21 +0100

    run eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,26 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import solid from "eslint-plugin-solid/configs/typescript";
 import * as tsParser from "@typescript-eslint/parser";
 import eslintConfigPrettier from "eslint-config-prettier";
+import solid from "eslint-plugin-solid/configs/typescript";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    files: ["**/*.{ts,tsx}"],
-    ...solid,
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        project: "tsconfig.json",
-      },
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ["**/*.{ts,tsx}"],
+        ...solid,
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                project: "tsconfig.json",
+            },
+        },
     },
-  },
-  eslintConfigPrettier,
+    eslintConfigPrettier,
+    { ignores: [".vinxi", ".output"] },
 ];
