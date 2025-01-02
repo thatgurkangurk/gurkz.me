b06447bb8c6dec40b9116e8cd420cb62da3f9131
Gurkan <hello@gurkz.me>
2025-01-02 14:46:31 +0100

    basic design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,13 @@ import type { Config } from "tailwindcss";
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                themeColour: "#5EBFA8",
+                themeBlack: "#121212",
+                themeGrey: "#3d3d3d",
+            },
+        },
     },
     plugins: [],
     darkMode: "selector",
