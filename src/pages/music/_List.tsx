ce3e6ae3464fdb8fc9f808c3c0ed388a3aa60322
Gurkan <hello@gurkz.me>
2025-01-07 14:02:44 +0100

    set max width on tabs

--- src/pages/music/_List.tsx
+++ src/pages/music/_List.tsx
@@ -18,7 +18,7 @@ export function MusicList(props: {
 
     return (
         <>
-            <Tabs value={format()} onChange={setFormat} class="w-[400px]">
+            <Tabs value={format()} onChange={setFormat} class="max-w-[400px]">
                 <TabsList class="grid w-full grid-cols-2">
                     <TabsTrigger value="DEFAULT">default</TabsTrigger>
                     <TabsTrigger value="TRAITOR_TOWN">traitor town</TabsTrigger>
