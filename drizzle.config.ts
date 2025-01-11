5727d0142f1aea36b45d0b5fc6e5ebff90e6d5aa
Gurkan <hello@gurkz.me>
2025-01-11 15:59:11 +0100

    [ci-skip] local postgres setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+const databaseUrl = process.env.REMOTE
+    ? process.env.REMOTE_DATABASE_URL
+    : process.env.DATABASE_URL;
+
 export default defineConfig({
     out: "./drizzle",
     schema: "./src/db/schema.ts",
     dialect: "postgresql",
     dbCredentials: {
-        url: process.env.DATABASE_URL!,
+        url: databaseUrl!,
     },
 });
