4afb6353805634a5d70c91b8fa4541c9277b13f1
Gurkan <hello@gurkz.me>
2025-01-22 11:57:25 +0100

    add 404 page

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from "@solidjs/start/config";
-import tailwindcss from "@tailwindcss/postcss";
+import tailwindcss from "@tailwindcss/vite";
+// import tailwindcss from "@tailwindcss/postcss";
 import lucidePreprocess from "vite-plugin-lucide-preprocess";
 
 export default defineConfig({
     ssr: true,
     vite: {
-        css: {
-            postcss: {
-                plugins: [tailwindcss],
-            },
-        },
-        plugins: [lucidePreprocess()],
+        // css: {
+        //     postcss: {
+        //         plugins: [tailwindcss],
+        //     },
+        // },
+        plugins: [lucidePreprocess(), tailwindcss()],
     },
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,10 @@
+import { Title } from "@solidjs/meta";
 import { ModeToggle } from "~/components/mode-toggle";
 
 export default function Home() {
     return (
         <>
+            <Title>home</Title>
             <h1 class="text-3xl">hello, world!</h1>
             <ModeToggle />
         </>
