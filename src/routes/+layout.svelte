1ffe8b4f393411791933e1d2207f68720b6fc582
Gurkan <hello@gurkz.me>
2025-02-17 21:00:15 +0100

    add dark mode to protect my eyes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import LightSwitch from "$lib/components/light-switch.svelte";
+	import "../app.css";
+	import { ModeWatcher } from "mode-watcher";
+	let { children } = $props();
+</script>
+
+<ModeWatcher />
+<LightSwitch />
+{@render children?.()}
