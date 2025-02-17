34d76ce99700e051f29d968998cce85648493f4f
Gurkan <hello@gurkz.me>
2025-02-17 22:22:22 +0100

    Skip environment validation when in CI

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,5 +4,6 @@ import { process } from "std-env";
 
 export const env = createEnv({
 	server: { DATABASE_URL: v.string() },
-	runtimeEnvStrict: { DATABASE_URL: process.env.DATABASE_URL }
+	runtimeEnvStrict: { DATABASE_URL: process.env.DATABASE_URL },
+	skipValidation: process.env.CI === "1"
 });
