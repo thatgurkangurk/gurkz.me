3d46fee851185064a6b4f8dde1cda7e85eab9a39
Gurkan <hello@gurkz.me>
2025-06-04 19:58:27 +0200

    remove QueryBoundary temporairly (caused issues in dev)

--- src/routes/music/index.tsx
+++ src/routes/music/index.tsx
@@ -1,8 +1,7 @@
 import { useInfiniteQuery } from "@tanstack/solid-query";
 import { FormatProvider } from "./_lib/format";
 import { orpc } from "~/lib/orpc";
-import { QueryBoundary } from "~/components/query-boundary";
-import { For, Show } from "solid-js";
+import { ErrorBoundary, For, Show, Suspense } from "solid-js";
 import { ClientOnly } from "solid-use/client-only";
 import { FormatSelector } from "./_lib/format-selector";
 import { Button } from "~/components/ui/button";
@@ -56,28 +55,30 @@ export default function MusicPage() {
 
 			<FormatSelector />
 
-			<QueryBoundary
-				query={query}
-				loadingFallback={<LoaderCircle size={48} class="animate-spin" />}
-			>
-				{(data) => (
-					<div class="px-2">
-						<div class="pt-4 grid grid-cols-1 sm:grid-cols-2 w-full place-items-center md:grid-cols-3 xl:grid-cols-5 gap-4">
-							<For each={data.pages}>
-								{(page) => (
-									<For each={page.data}>{(musicId) => <MusicCard musicId={musicId} />}</For>
-								)}
-							</For>
-						</div>
+			<Suspense fallback={<LoaderCircle size={48} class="animate-spin" />}>
+				<ErrorBoundary fallback={() => <p>something went wrong</p>}>
+					<Show
+						when={query.isSuccess && query.data}
+						fallback={<LoaderCircle size={48} class="animate-spin" />}
+					>
+						<div class="px-2">
+							<div class="pt-4 grid grid-cols-1 sm:grid-cols-2 w-full place-items-center md:grid-cols-3 xl:grid-cols-5 gap-4">
+								<For each={query.data?.pages}>
+									{(page) => (
+										<For each={page.data}>{(musicId) => <MusicCard musicId={musicId} />}</For>
+									)}
+								</For>
+							</div>
 
-						<LoadMoreButton
-							hasNextPage={query.hasNextPage}
-							isFetchingNextPage={query.isFetchingNextPage}
-							fetchNextPage={query.fetchNextPage}
-						/>
-					</div>
-				)}
-			</QueryBoundary>
+							<LoadMoreButton
+								hasNextPage={query.hasNextPage}
+								isFetchingNextPage={query.isFetchingNextPage}
+								fetchNextPage={query.fetchNextPage}
+							/>
+						</div>
+					</Show>
+				</ErrorBoundary>
+			</Suspense>
 		</FormatProvider>
 	);
 }
