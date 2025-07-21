5913cc3b7891fefcfaefa2713a146b76000c336c
Gurkan <hello@gurkz.me>
2025-07-21 17:44:13 +0200

    svelte-adapter-bun-next
    Signed-off-by: Gurkan <hello@gurkz.me>

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "svelte-adapter-bun-next";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,11 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({
+			precompress: true
+		})
+	}
 };
 
 export default config;
