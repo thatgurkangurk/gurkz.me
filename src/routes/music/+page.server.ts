800750cf7a364bc5c54b1e1d10e365d53c2f5310
Gurkan <hello@gurkz.me>
2025-07-21 17:39:56 +0200

    remove console.log
    Signed-off-by: Gurkan <hello@gurkz.me>

--- src/routes/music/+page.server.ts
+++ src/routes/music/+page.server.ts
@@ -1,9 +1,7 @@
 import { db } from "$lib/server/db";
 
 export async function load() {
-	console.log("load?");
 	const musicIds = await db.query.musicIds.findMany();
-	console.log(musicIds);
 
 	return {
 		musicIds: musicIds
