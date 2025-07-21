72fd6e7b4285a080b7490af6c2dd7024e8318e06
Gurkan <hello@gurkz.me>
2025-07-21 18:00:47 +0200

    svelte config in typescript
    Signed-off-by: Gurkan <hello@gurkz.me>

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,7 +1,7 @@
 import adapter from "svelte-adapter-bun-next";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import type { Config } from "@sveltejs/kit";
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
@@ -11,6 +11,6 @@ const config = {
 			precompress: true
 		})
 	}
-};
+} satisfies Config;
 
 export default config;
