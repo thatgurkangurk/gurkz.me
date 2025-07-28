ced4c9702d9d0ae490254c889e2f69a0b078586f
Gurkan <hello@gurkz.me>
2025-07-28 18:06:06 +0200

    rollup bundle analyser
    Signed-off-by: Gurkan <hello@gurkz.me>

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import devtoolsJson from "vite-plugin-devtools-json";
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson(), visualizer()]
 });
