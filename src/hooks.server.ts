198922077664f7f70cc5989b5f70033c96a653ee
Gurkan <hello@gurkz.me>
2025-08-02 18:10:31 +0200

    fix hooks
    Signed-off-by: Gurkan <hello@gurkz.me>

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,7 @@
 import { auth } from "$lib/server/auth";
+import { building } from "$app/environment";
 import { svelteKitHandler } from "better-auth/svelte-kit";
- 
+
 export async function handle({ event, resolve }) {
-	return svelteKitHandler({ event, resolve, auth });
+	return svelteKitHandler({ event, resolve, auth, building });
 }
