7ec0ce154284c9b0f36a948882bbe703cb4a5306
Gurkan <hello@gurkz.me>
2025-09-03 15:54:13 +0200

    use https://shadcn-tanstack-form.netlify.app/ for the forms

--- src/components/music/edit-form.tsx
+++ src/components/music/edit-form.tsx
@@ -2,6 +2,7 @@ import {
   Dialog,
   DialogClose,
   DialogContent,
+  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -9,65 +10,51 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { MusicIdWithCreator, UpdateMusicIdInput } from "@/lib/schemas/music";
-import { ReactNode, useEffect, useMemo, useState } from "react";
-import { useAppForm } from "./form/hooks";
-import { FieldInfo } from "../form/field-info";
+import { useCallback, useMemo, useState } from "react";
+import { useAppForm } from "@/components/ui/tanstack-form";
 import { LoaderCircle, Trash } from "lucide-react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { orpc } from "@/lib/orpc";
 import { toast } from "sonner";
 import { useStore } from "@tanstack/react-form";
 import { useSession } from "@/hooks/useSession";
+import { Input } from "../ui/input";
+import { Switch } from "@/components/ui/switch";
+import { deepEqual } from "@tanstack/react-router";
 
-function FormWrapper({
-  children,
-  open,
-  setOpen,
-}: {
-  children: ReactNode;
-  open: boolean;
-  setOpen: (val: boolean) => void;
-}) {
+export function EditMusicIdForm({ musicId }: { musicId: MusicIdWithCreator }) {
+  const [open, setOpen] = useState(false);
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Button variant={"secondary"}>edit</Button>
       </DialogTrigger>
-      <DialogContent>{children}</DialogContent>
+      <DialogContent>
+        <InnerForm onSuccess={() => setOpen(false)} musicId={musicId} />
+      </DialogContent>
+      <DialogDescription className="sr-only">
+        form to edit a music id
+      </DialogDescription>
     </Dialog>
   );
 }
 
-function deepEqual(a: any, b: any): boolean {
-  if (a === b) return true;
-  if (typeof a !== typeof b) return false;
-
-  if (Array.isArray(a) && Array.isArray(b)) {
-    if (a.length !== b.length) return false;
-    return a.every((val, i) => deepEqual(val, b[i]));
-  }
-
-  if (a && b && typeof a === "object") {
-    const aKeys = Object.keys(a);
-    const bKeys = Object.keys(b);
-    if (aKeys.length !== bKeys.length) return false;
-    return aKeys.every((key) => deepEqual(a[key], b[key]));
-  }
-
-  return false;
-}
-
-export function EditMusicIdForm({ musicId }: { musicId: MusicIdWithCreator }) {
+function InnerForm({
+  musicId,
+  onSuccess,
+}: {
+  musicId: MusicIdWithCreator;
+  onSuccess: () => void;
+}) {
   const { data: user } = useSession();
-  const [open, setOpen] = useState(false);
   const { mutateAsync, isPending } = useMutation(
     orpc.music.update.mutationOptions({
       onSuccess: async () => {
         await queryClient.refetchQueries({
           queryKey: orpc.music.get.queryKey(),
         });
-
-        setOpen(false);
+        form.reset();
+        onSuccess();
       },
     })
   );
@@ -86,148 +73,184 @@ export function EditMusicIdForm({ musicId }: { musicId: MusicIdWithCreator }) {
     return !deepEqual(values, musicId);
   }, [values]);
 
-  useEffect(() => {
-    if (open) {
-      form.reset();
-    }
-  }, [open]);
+  const handleSave = useCallback(() => {
+    const promise = mutateAsync({ ...form.state.values, id: musicId.id });
+    toast.promise(promise, {
+      loading: "saving...",
+      success: `successfully saved "${musicId.name}"`,
+      error: "something went wrong",
+    });
+  }, [form, mutateAsync, musicId]);
 
   return (
-    <FormWrapper open={open} setOpen={setOpen}>
+    <>
       <DialogHeader>
         <DialogTitle>editing "{musicId.name}"</DialogTitle>
       </DialogHeader>
       <div className="flex items-center gap-2">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-          }}
-          method="post"
-          encType={"multipart/form-data"}
-        >
-          <form.AppForm>
-            <form.AppField
-              name="name"
-              children={(field) => (
-                <>
-                  <field.TextField label="display name" />
-                  <FieldInfo field={field} />
-                </>
-              )}
-            />
-            <form.AppField
-              name="robloxId"
-              children={(field) => (
-                <>
-                  <field.TextField label="roblox id" />
-                  <FieldInfo field={field} />
-                </>
-              )}
-            />
-            <form.Field name="tags" mode="array">
-              {(field) => {
-                return (
-                  <div>
-                    {field.state.value?.map((_, i) => {
-                      return (
-                        <form.AppField
-                          key={i}
-                          name={`tags[${i}]`}
-                          children={(subField) => (
-                            <>
-                              <div className="flex w-full max-w-sm items-center gap-2">
-                                <subField.TextFieldWithButton
-                                  buttonProps={{
-                                    className: "mt-8",
-                                    type: "button",
-                                    variant: "destructive",
-                                    size: "icon",
-                                    children: <Trash />,
-                                    onClick: () => {
-                                      field.removeValue(i);
-                                    },
-                                  }}
-                                  label={`tag ${i + 1}`}
-                                />
-                              </div>
-
-                              <FieldInfo field={subField} />
-                            </>
-                          )}
-                        />
-                      );
-                    })}
-                    <Button
-                      disabled={(field.state.value?.length ?? 0) > 3}
-                      onClick={() => field.pushValue("")}
-                      type="button"
-                      className="my-2"
-                      variant={"secondary"}
-                    >
-                      {(field.state.value?.length ?? 0) > 3
-                        ? "max tags reached"
-                        : "add tag"}
-                    </Button>
-                  </div>
-                );
-              }}
-            </form.Field>
-
-            <form.AppField
-              name="working"
-              children={(field) => (
-                <>
-                  <field.SwitchField label="working" />
-                  <FieldInfo field={field} />
-                </>
-              )}
-            />
-
-            {user?.user.permissions.includes("MANAGE_MUSIC_IDS") && (
+        <form.AppForm>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+            }}
+            method="post"
+            encType={"multipart/form-data"}
+          >
+            <div className="space-y-4">
               <form.AppField
-                name="verified"
+                name="name"
+                children={(field) => (
+                  <field.FormItem>
+                    <field.FormLabel>display name</field.FormLabel>
+                    <field.FormControl>
+                      <Input
+                        aria-invalid={!field.state.meta.isValid}
+                        name={field.name}
+                        value={field.state.value}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        onBlur={field.handleBlur}
+                      />
+                    </field.FormControl>
+                    <field.FormMessage />
+                  </field.FormItem>
+                )}
+              />
+              <form.AppField
+                name="robloxId"
+                children={(field) => (
+                  <field.FormItem>
+                    <field.FormLabel>roblox id</field.FormLabel>
+                    <field.FormControl>
+                      <Input
+                        aria-invalid={!field.state.meta.isValid}
+                        name={field.name}
+                        value={field.state.value}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        onBlur={field.handleBlur}
+                      />
+                    </field.FormControl>
+                    <field.FormMessage />
+                  </field.FormItem>
+                )}
+              />
+              <form.AppField name="tags" mode="array">
+                {(field) => {
+                  return (
+                    <div>
+                      {field.state.value?.map((_, i) => {
+                        return (
+                          <form.AppField
+                            key={i}
+                            name={`tags[${i}]`}
+                            children={(subField) => (
+                              <div className="flex w-full max-w-sm items-center gap-2">
+                                <subField.FormItem>
+                                  <subField.FormLabel>
+                                    tag {i + 1}
+                                  </subField.FormLabel>
+                                  <div className="flex w-full max-w-sm items-center gap-2">
+                                    <subField.FormControl className="flex-1">
+                                      <Input
+                                        aria-invalid={
+                                          !subField.state.meta.isValid
+                                        }
+                                        name={subField.name}
+                                        value={subField.state.value}
+                                        onChange={(e) =>
+                                          subField.handleChange(e.target.value)
+                                        }
+                                        onBlur={subField.handleBlur}
+                                      />
+                                    </subField.FormControl>
+
+                                    <Button
+                                      type="button"
+                                      variant="destructive"
+                                      size="icon"
+                                      onClick={() => field.removeValue(i)}
+                                    >
+                                      <Trash />
+                                    </Button>
+                                  </div>
+                                  <subField.FormMessage />
+                                </subField.FormItem>
+                              </div>
+                            )}
+                          />
+                        );
+                      })}
+                      <Button
+                        disabled={(field.state.value?.length ?? 0) > 3}
+                        onClick={() => field.pushValue("")}
+                        type="button"
+                        className="my-2"
+                        variant={"secondary"}
+                      >
+                        {(field.state.value?.length ?? 0) > 3
+                          ? "max tags reached"
+                          : "add tag"}
+                      </Button>
+                    </div>
+                  );
+                }}
+              </form.AppField>
+
+              <form.AppField
+                name="working"
                 children={(field) => (
                   <>
-                    <field.SwitchField label="verified" />
-                    <FieldInfo field={field} />
+                    <field.FormItem>
+                      <field.FormLabel>working</field.FormLabel>
+                      <field.FormControl>
+                        <Switch
+                          aria-invalid={!field.state.meta.isValid}
+                          name={field.name}
+                          checked={field.state.value}
+                          onCheckedChange={(value) => field.handleChange(value)}
+                          onBlur={field.handleBlur}
+                        />
+                      </field.FormControl>
+                      <field.FormMessage />
+                    </field.FormItem>
                   </>
                 )}
               />
-            )}
-          </form.AppForm>
-        </form>
+
+              {user?.user.permissions.includes("MANAGE_MUSIC_IDS") && (
+                <form.AppField name="verified">
+                  {(field) => (
+                    <field.FormItem>
+                      <field.FormLabel>verified</field.FormLabel>
+                      <field.FormControl>
+                        <Switch
+                          aria-invalid={!field.state.meta.isValid}
+                          name={field.name}
+                          checked={field.state.value}
+                          onCheckedChange={(value) => field.handleChange(value)}
+                          onBlur={field.handleBlur}
+                        />
+                      </field.FormControl>
+                      <field.FormMessage />
+                    </field.FormItem>
+                  )}
+                </form.AppField>
+              )}
+            </div>
+          </form>
+        </form.AppForm>
       </div>
 
       <DialogFooter className="grid grid-cols-2">
-        <Button
-          disabled={isPending || !hasChanges}
-          onClick={() => {
-            const promise = mutateAsync({
-              ...form.state.values,
-              id: musicId.id,
-            });
-            toast.promise(promise, {
-              loading: "saving...",
-              success: `successfully saved "${musicId.name}"`,
-              error: "something went wrong",
-            });
-          }}
-        >
-          {isPending && <LoaderCircle className="animate-spin" />}
-          save
+        <Button disabled={isPending || !hasChanges} onClick={handleSave}>
+          {isPending && <LoaderCircle className="animate-spin" />} save
         </Button>
         <DialogClose asChild>
-          <Button
-            disabled={isPending}
-            onClick={() => setOpen(false)}
-            type="button"
-            variant={"secondary"}
-          >
+          <Button disabled={isPending} type="button" variant="secondary">
             cancel
           </Button>
         </DialogClose>
       </DialogFooter>
-    </FormWrapper>
+    </>
   );
 }
--- src/components/music/form/form.tsx
+++ src/components/music/form/form.tsx
@@ -1,12 +1,15 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useAppForm } from "./hooks";
+import { useAppForm } from "@/components/ui/tanstack-form";
 import { formOpts } from "./options";
 import { orpc } from "@/lib/orpc";
-import { FieldInfo } from "@/components/form/field-info";
-import { Trash } from "lucide-react";
+import { LoaderCircle, Trash } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import type { ReactNode } from "react";
+import { useCallback, useMemo, type ReactNode } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
+import { useStore } from "@tanstack/react-form";
+import { deepEqual } from "@tanstack/react-router";
 
 function FormWrapper({ children }: { children: ReactNode }) {
   return (
@@ -20,14 +23,14 @@ function FormWrapper({ children }: { children: ReactNode }) {
 }
 
 export function CreateMusicIdForm() {
-  const createMusicIdMutation = useMutation(
+  const { mutateAsync, isPending } = useMutation(
     orpc.music.create.mutationOptions()
   );
   const queryClient = useQueryClient();
   const form = useAppForm({
     ...formOpts,
     onSubmit: async ({ formApi, value }) => {
-      await createMusicIdMutation.mutateAsync(value);
+      await mutateAsync(value);
 
       await queryClient.refetchQueries({
         queryKey: orpc.music.get.queryKey(),
@@ -36,87 +39,145 @@ export function CreateMusicIdForm() {
       formApi.reset();
     },
   });
+  const handleSubmit = useCallback(() => {
+    const promise = mutateAsync(form.state.values);
+    toast.promise(promise, {
+      loading: "creating...",
+      success: `successfully created`,
+      error: "something went wrong",
+    });
+  }, [form, mutateAsync]);
+
+  const formValues = useStore(form.store, (state) => state.values);
+  const hasChanges = useMemo(() => {
+    return !deepEqual(formValues, form.options.defaultValues);
+  }, [formValues]);
+
   return (
     <FormWrapper>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-        }}
-        method="post"
-        encType={"multipart/form-data"}
-      >
+      <div className="flex items-center gap-2">
         <form.AppForm>
-          <form.AppField
-            name="name"
-            children={(field) => (
-              <>
-                <field.TextField label="name" />
-                <FieldInfo field={field} />
-              </>
-            )}
-          />
-          <form.AppField
-            name="robloxId"
-            children={(field) => (
-              <>
-                <field.TextField label="roblox id" />
-                <FieldInfo field={field} />
-              </>
-            )}
-          />
-          <form.Field name="tags" mode="array">
-            {(field) => {
-              return (
-                <div>
-                  {field.state.value.map((_, i) => {
-                    return (
-                      <form.AppField
-                        key={i}
-                        name={`tags[${i}]`}
-                        children={(subField) => (
-                          <>
-                            <div className="flex w-full max-w-sm items-center gap-2">
-                              <subField.TextFieldWithButton
-                                buttonProps={{
-                                  className: "mt-8",
-                                  type: "button",
-                                  variant: "destructive",
-                                  size: "icon",
-                                  children: <Trash />,
-                                  onClick: () => {
-                                    field.removeValue(i);
-                                  },
-                                }}
-                                label={`tag ${i + 1}`}
-                              />
-                            </div>
-
-                            <FieldInfo field={subField} />
-                          </>
-                        )}
-                      />
-                    );
-                  })}
-                  <Button
-                    disabled={field.state.value.length > 3}
-                    onClick={() => field.pushValue("")}
-                    type="button"
-                    className="my-2"
-                    variant={"secondary"}
-                  >
-                    {field.state.value.length > 3
-                      ? "max tags reached"
-                      : "add tag"}
-                  </Button>
-                </div>
-              );
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
             }}
-          </form.Field>
+            method="post"
+            encType={"multipart/form-data"}
+          >
+            <div className="space-y-4">
+              <form.AppField
+                name="name"
+                children={(field) => (
+                  <field.FormItem>
+                    <field.FormLabel>display name</field.FormLabel>
+                    <field.FormControl>
+                      <Input
+                        aria-invalid={!field.state.meta.isValid}
+                        name={field.name}
+                        value={field.state.value}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        onBlur={field.handleBlur}
+                      />
+                    </field.FormControl>
+                    <field.FormMessage />
+                  </field.FormItem>
+                )}
+              />
+              <form.AppField
+                name="robloxId"
+                children={(field) => (
+                  <field.FormItem>
+                    <field.FormLabel>roblox id</field.FormLabel>
+                    <field.FormControl>
+                      <Input
+                        aria-invalid={!field.state.meta.isValid}
+                        name={field.name}
+                        value={field.state.value}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        onBlur={field.handleBlur}
+                      />
+                    </field.FormControl>
+                    <field.FormMessage />
+                  </field.FormItem>
+                )}
+              />
+              <form.AppField name="tags" mode="array">
+                {(field) => {
+                  return (
+                    <div>
+                      {field.state.value?.map((_, i) => {
+                        return (
+                          <form.AppField
+                            key={i}
+                            name={`tags[${i}]`}
+                            children={(subField) => (
+                              <div className="flex w-full max-w-sm items-center gap-2">
+                                <subField.FormItem>
+                                  <subField.FormLabel>
+                                    tag {i + 1}
+                                  </subField.FormLabel>
+                                  <div className="flex w-full max-w-sm items-center gap-2">
+                                    <subField.FormControl className="flex-1">
+                                      <Input
+                                        aria-invalid={
+                                          !subField.state.meta.isValid
+                                        }
+                                        name={subField.name}
+                                        value={subField.state.value}
+                                        onChange={(e) =>
+                                          subField.handleChange(e.target.value)
+                                        }
+                                        onBlur={subField.handleBlur}
+                                      />
+                                    </subField.FormControl>
 
-          <form.SubscribeButton label="submit" />
+                                    <Button
+                                      type="button"
+                                      variant="destructive"
+                                      size="icon"
+                                      onClick={() => field.removeValue(i)}
+                                    >
+                                      <Trash />
+                                    </Button>
+                                  </div>
+                                  <subField.FormMessage />
+                                </subField.FormItem>
+                              </div>
+                            )}
+                          />
+                        );
+                      })}
+                      <Button
+                        disabled={(field.state.value?.length ?? 0) > 3}
+                        onClick={() => field.pushValue("")}
+                        type="button"
+                        className="my-2"
+                        variant={"secondary"}
+                      >
+                        {(field.state.value?.length ?? 0) > 3
+                          ? "max tags reached"
+                          : "add tag"}
+                      </Button>
+                    </div>
+                  );
+                }}
+              </form.AppField>
+            </div>
+            <form.Subscribe selector={(state) => state.isValid}>
+              {(isValid) => (
+                <Button
+                  disabled={isPending || !hasChanges || !isValid}
+                  onClick={handleSubmit}
+                >
+                  {isPending && <LoaderCircle className="animate-spin" />}{" "}
+                  create
+                </Button>
+              )}
+            </form.Subscribe>
+          </form>
         </form.AppForm>
-      </form>
+      </div>
     </FormWrapper>
   );
 }
