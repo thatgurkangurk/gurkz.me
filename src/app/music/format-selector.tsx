e92797ffbe1de8e8c8dc229764ace69437e9f259
Gurkan <hello@gurkz.me>
2025-09-10 15:10:42 +0200

    remove repeated button

--- src/app/music/format-selector.tsx
+++ src/app/music/format-selector.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useAtom } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
 import { type IdFormat, idFormatAtom } from "./id-format";
 import { Button } from "@/components/ui/button";
 import { useHydrateAtoms } from "jotai/utils";
@@ -9,30 +9,31 @@ type FormatSelectorProps = Readonly<{
   idFormatFromServer?: IdFormat;
 }>;
 
+function SelectButton({ format, label }: { format: IdFormat; label: string }) {
+  const [idFormat, setIdFormat] = useAtom(idFormatAtom);
+  return (
+    <Button
+      formAction={setIdFormatAction.bind(null, format)}
+      disabled={idFormat === format}
+      onClick={() => setIdFormat(format)}
+    >
+      {label}
+    </Button>
+  );
+}
+
 export function FormatSelector({
   idFormatFromServer = "DEFAULT",
 }: FormatSelectorProps) {
   useHydrateAtoms([[idFormatAtom, idFormatFromServer]]);
-  const [idFormat, setIdFormat] = useAtom(idFormatAtom);
+  const idFormat = useAtomValue(idFormatAtom);
 
   return (
     <>
       <p>current format: {idFormat}</p>
       <form className="flex flex-row gap-2">
-        <Button
-          formAction={setIdFormatAction.bind(null, "DEFAULT")}
-          disabled={idFormat === "DEFAULT"}
-          onClick={() => setIdFormat("DEFAULT")}
-        >
-          default
-        </Button>
-        <Button
-          formAction={setIdFormatAction.bind(null, "TRAITOR_TOWN")}
-          disabled={idFormat === "TRAITOR_TOWN"}
-          onClick={() => setIdFormat("TRAITOR_TOWN")}
-        >
-          traitor town
-        </Button>
+        <SelectButton format="DEFAULT" label="default" />
+        <SelectButton format="TRAITOR_TOWN" label="traitor town" />
       </form>
     </>
   );
