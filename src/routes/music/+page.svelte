282217e025ce2d4f9db4324705ec2486df848bfd
Gurkan <hello@gurkz.me>
2025-09-27 16:18:04 +0200

    fix copy button

--- src/routes/music/+page.svelte
+++ src/routes/music/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import CopyButton from "$lib/components/copy-button.svelte";
 	import FormatSelector from "./components/format-selector.svelte";
-	import FormattedId from "./components/formatted-id.svelte";
+	import FormattedId, { formatId } from "./components/formatted-id.svelte";
+	import { IdFormatContext } from "./context";
 	import { getMusicIds } from "./music.remote";
+
+	const format = IdFormatContext.get();
 </script>
 
 <p>music id list</p>
@@ -12,6 +15,6 @@
 {#each await getMusicIds() as musicId (musicId.id)}
 	<div class="flex items-center gap-2">
 		<p>{musicId.name} - <FormattedId robloxId={musicId.robloxId} /></p>
-		<CopyButton variant={"outline"} text={musicId.robloxId} />
+		<CopyButton variant={"outline"} text={formatId(musicId.robloxId, format.current)} />
 	</div>
 {/each}
