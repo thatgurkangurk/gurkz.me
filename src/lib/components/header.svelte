9dbc3c3952345fe7099fe42b65c17085d804db17
Gurkan <hello@gurkz.me>
2025-09-27 18:53:56 +0200

    typed routes

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import { resolve } from "$app/paths";
+	import { route } from "$lib/ROUTES";
 	import ModeToggle from "./mode-toggle.svelte";
 	import NavLink from "./nav-link.svelte";
 </script>
 
 <nav class="flex w-full items-center gap-2 p-2">
-	<NavLink activeClass="underline underline-offset-2 decoration-lime-500" href="/">home</NavLink>
-	<NavLink activeClass="underline underline-offset-2 decoration-lime-500" href="/music">
+	<NavLink activeClass="underline underline-offset-2 decoration-lime-500" href={route("/")}>
+		home
+	</NavLink>
+	<NavLink activeClass="underline underline-offset-2 decoration-lime-500" href={route("/music")}>
 		music id list
 	</NavLink>
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import devtoolsJson from "vite-plugin-devtools-json";
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { kitRoutes } from "vite-plugin-kit-routes";
+import type { KIT_ROUTES } from "$lib/ROUTES.js";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
+	plugins: [kitRoutes<KIT_ROUTES>(), tailwindcss(), sveltekit(), devtoolsJson()]
 });
