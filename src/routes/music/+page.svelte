3e684c0fe27ffd1e89cf5cef5342e17f74955845
Gurkan <hello@gurkz.me>
2025-09-28 13:26:45 +0200

    fix hydration mismatches

--- src/routes/music/+page.svelte
+++ src/routes/music/+page.svelte
@@ -2,6 +2,8 @@
 	import FormatSelector from "./components/format-selector.svelte";
 	import MusicCard from "./components/music-card.svelte";
 	import { getMusicIds } from "./music.remote";
+
+	const musicIds = await getMusicIds();
 </script>
 
 <p>music id list</p>
@@ -11,7 +13,7 @@
 <div
 	class="grid w-full grid-cols-1 place-items-center gap-4 pt-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5"
 >
-	{#each await getMusicIds() as musicId (musicId.id)}
+	{#each musicIds as musicId (musicId.id)}
 		<MusicCard {musicId} />
 	{/each}
 </div>
