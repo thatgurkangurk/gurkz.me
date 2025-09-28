529849030547e0c4bb4bbf89a5beb23e6af18d4a
Gurkan <hello@gurkz.me>
2025-09-28 14:18:27 +0200

    using resolve() breaks active styling

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
-	import { resolve } from "$app/paths";
 	import ModeToggle from "./mode-toggle.svelte";
 	import NavLink from "./nav-link.svelte";
 </script>
 
 <nav class="flex w-full items-center gap-2 p-2">
-	<NavLink activeClass="underline underline-offset-2 decoration-lime-500" href={resolve("/")}>
-		home
-	</NavLink>
-	<NavLink activeClass="underline underline-offset-2 decoration-lime-500" href={resolve("/music")}>
+	<NavLink activeClass="underline underline-offset-2 decoration-lime-500" href="/">home</NavLink>
+	<NavLink activeClass="underline underline-offset-2 decoration-lime-500" href="/music">
 		music id list
 	</NavLink>
 
