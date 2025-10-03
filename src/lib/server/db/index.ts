946b484d57a94dc759cec8ad6a6f560799d45f5b
Gurkan <hello@gurkz.me>
2025-10-03 16:40:53 +0200

    fix

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -4,8 +4,6 @@ import * as authSchema from "./schema/auth";
 import * as musicSchema from "./schema/music";
 import * as permissionsSchema from "./schema/permission";
 
-if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
-
 const schema = { ...authSchema, ...musicSchema, ...permissionsSchema };
 
-export const db = drizzle(env.DATABASE_URL, { schema });
+export const db = drizzle(env.DATABASE_URL!, { schema });
