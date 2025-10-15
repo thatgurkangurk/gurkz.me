b15cd30631f1885a9bc884d2d6e6135884ca589c
Gurkan <hello@gurkz.me>
2025-10-15 19:54:35 +0200

    remove all uses of tanstack query (not needed)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,21 +5,16 @@
 	import Header from "$lib/components/header/header.svelte";
 	import type { LayoutProps } from "./$types";
 	import { TooltipProvider } from "$lib/components/ui/tooltip";
-	import { dehydrate, QueryClientProvider } from "@tanstack/svelte-query";
-	import { SvelteQueryDevtools } from "@tanstack/svelte-query-devtools";
 	import { Toaster } from "$lib/components/ui/sonner/index.js";
 
 	let { children, data }: LayoutProps = $props();
 </script>
 
-<QueryClientProvider client={data.queryClient}>
-	<ModeWatcher />
-	<Header />
-	<Toaster />
-	<main class="p-2">
-		<TooltipProvider>
-			{@render children()}
-		</TooltipProvider>
-	</main>
-	<SvelteQueryDevtools />
-</QueryClientProvider>
+<ModeWatcher />
+<Header />
+<Toaster />
+<main class="p-2">
+	<TooltipProvider>
+		{@render children()}
+	</TooltipProvider>
+</main>
