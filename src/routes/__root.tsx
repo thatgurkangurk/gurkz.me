e93055be6d12b365b42dee6b3caa7166598af8dc
Gurkan <hello@gurkz.me>
2025-11-29 23:35:33 +0100

    better navbar

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,6 +5,7 @@ import {
   createRootRoute,
   HeadContent,
   Scripts,
+  Link,
 } from "@tanstack/react-router";
 import globalCss from "../styles/global.css?url";
 
@@ -43,6 +44,10 @@ function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
           <HeadContent />
         </head>
         <body>
+          <nav className="flex flex-row w-full gap-2 bg-orange-400">
+            <Link to="/">home</Link>
+            <Link to="/music">music id list</Link>
+          </nav>
           {children}
           <Scripts />
         </body>
