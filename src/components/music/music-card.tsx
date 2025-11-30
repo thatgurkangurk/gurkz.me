a71bda8f3034b8bdf88db4e410f1821bae300879
Gurkan <hello@gurkz.me>
2025-11-30 22:04:48 +0100

    show creator on the music card

--- src/components/music/music-card.tsx
+++ src/components/music/music-card.tsx
@@ -1,6 +1,16 @@
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 import type { MusicIdWithCreator } from "~/server/db/schema/music";
 import { FormattedId } from "./formatted-id";
+
+const dateFormat = new Intl.DateTimeFormat("en-GB", {
+  dateStyle: "long",
+});
 
 export function MusicCard(
   props: Readonly<{
@@ -18,6 +28,14 @@ export function MusicCard(
           <FormattedId robloxId={props.musicId.robloxId} />
         </div>
       </CardContent>
+      <CardFooter className="grid grid-cols-1 gap-1">
+        <p>
+          created by <span>{props.musicId.creator.name}</span> on{" "}
+          <span className="whitespace-nowrap">
+            {dateFormat.format(props.musicId.created)}
+          </span>
+        </p>
+      </CardFooter>
     </Card>
   );
 }
