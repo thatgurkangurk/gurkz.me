5384168982c3cdf3204bc15df648d4d2bc709e76
Gurkan <hello@gurkz.me>
2025-12-17 16:24:26 +0100

    fix

--- src/routes/admin/user/$userId.tsx
+++ src/routes/admin/user/$userId.tsx
@@ -14,6 +14,7 @@ import { Checkbox } from "~/components/ui/checkbox";
 import { Field, FieldGroup, FieldSet } from "~/components/ui/field";
 import type { User } from "~/lib/auth";
 import { type Permission, permissions } from "~/lib/permissions";
+import { getServerSession } from "~/lib/session";
 
 function UserPermissionsSelector({
   user,
