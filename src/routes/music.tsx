6eb31bbc530f95b4122016a6a0c92cf1e3723b5b
Gurkan <hello@gurkz.me>
2025-12-25 18:33:20 +0100

    remove notice about sound player being broken

--- src/routes/music.tsx
+++ src/routes/music.tsx
@@ -3,7 +3,6 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 import { createIsomorphicFn } from "@tanstack/react-start";
 import { getCookie } from "@tanstack/react-start/server";
 import * as cookie from "cookie";
-import { useAtomValue } from "jotai";
 import { useHydrateAtoms } from "jotai/utils";
 import { CreateMusicIdForm } from "~/components/music/create-form";
 import { idFormat, idFormatSchema } from "~/components/music/format";
@@ -12,7 +11,6 @@ import { MusicCard } from "~/components/music/music-card";
 import { orpc } from "~/lib/orpc";
 import { Check } from "~/lib/permix";
 import { getServerSession } from "~/lib/session";
-import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
 import { AlertCircleIcon } from "lucide-react";
 
 const getInitialIdFormat = createIsomorphicFn()
@@ -65,29 +63,17 @@ export const Route = createFileRoute("/music")({
 function RouteComponent() {
   const { data } = useSuspenseQuery(orpc.music.list.queryOptions());
   const { initialIdFormat } = Route.useLoaderData();
-  const format = useAtomValue(idFormat);
 
   useHydrateAtoms([[idFormat, initialIdFormat]]);
 
   return (
     <div>
       music id list
-      {format === "DEFAULT" && <p>no</p>}
       <Check entity={"musicId"} action={"create"}>
         <CreateMusicIdForm />
       </Check>
       <FormatSelector />
       <br />
-      <Alert variant="destructive">
-        <AlertCircleIcon />
-        <AlertTitle>warning</AlertTitle>
-        <AlertDescription>
-          <p>
-            unfortunately, chat in Traitor Town has been disabled, and as such,
-            as of now, the sound player gamepass does not work.
-          </p>
-        </AlertDescription>
-      </Alert>
       <div className="grid w-full grid-cols-1 place-items-center gap-4 pt-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
         {data.map((musicId) => (
           <MusicCard key={musicId.id} musicId={musicId} />
