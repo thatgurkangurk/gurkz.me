06e4f7228f52f43e3a16158e0cbf9259ae871cc5
Gurkan <hello@gurkz.me>
2026-01-02 13:59:15 +0100

    music id card

--- src/routes/music/+page.svelte
+++ src/routes/music/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import type { PageProps } from "./$types";
+	import MusicCard from "./components/music-card.svelte";
 
 	let { data }: PageProps = $props();
 </script>
 
 <h1 class="text-3xl">music id list</h1>
 
-{#each data.musicIds as musicId (musicId.id)}
-	<div class="flex flex-row gap-2">
-		<p>{musicId.name} - s/{musicId.robloxId}</p>
-		<button onclick={() => navigator.clipboard.writeText(`s/${musicId.robloxId}`)}>copy</button>
-	</div>
-{/each}
+<div
+	class="grid w-full grid-cols-1 place-items-center gap-4 pt-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5"
+>
+	{#each data.musicIds as musicId (musicId.id)}
+		<MusicCard {musicId} />
+	{/each}
+</div>
---
+++ src/routes/music/components/music-card.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import {
+		Card,
+		CardContent,
+		CardFooter,
+		CardHeader,
+		CardTitle
+	} from "$lib/components/ui/card/index.js";
+	import { CopyButton } from "$lib/components/ui/copy-button";
+	import type { MusicIdWithCreator } from "$lib/server/db/schema/music";
+
+	type Props = {
+		musicId: MusicIdWithCreator;
+	};
+
+	const dateFormat = new Intl.DateTimeFormat("en-GB", {
+		dateStyle: "long"
+	});
+
+	let { musicId }: Props = $props();
+</script>
+
+<Card class="h-full w-full">
+	<CardHeader>
+		<CardTitle class="text-xl">{musicId.name}</CardTitle>
+	</CardHeader>
+
+	<CardContent>
+		<div class="flex items-center gap-2 text-xl">
+			<span>s/{musicId.robloxId}</span>
+			<CopyButton text="s/{musicId.robloxId}" variant="outline" />
+		</div>
+	</CardContent>
+	<CardFooter class="grid grid-cols-1 gap-1">
+		<div class="flex flex-row gap-2">
+			<!-- delete music card -->
+		</div>
+		<p>
+			created by <span>{musicId.creator.name}</span> on{" "}
+			<span class="whitespace-nowrap">
+				{dateFormat.format(musicId.created)}
+			</span>
+		</p>
+	</CardFooter>
+</Card>
