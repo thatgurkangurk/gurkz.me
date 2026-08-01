d1847fc6504f22aeb5175a7f333ff93e3cd4958d
Gurkan <hello@gurkz.me>
2026-08-01 21:05:03 +0200

    remove redundant stuff

--- src/routes/admin/users/[userId]/schemas.ts
+++ src/routes/admin/users/[userId]/schemas.ts
@@ -7,13 +7,10 @@ export const nonDefaultPermissions = permissions.filter(
 	(p): p is NonDefaultPermission => p !== "DEFAULT"
 ) as [NonDefaultPermission, ...NonDefaultPermission[]];
 
-const NonDefaultPermissionsEnum = z.enum(nonDefaultPermissions);
-
 const permissionShape = Object.fromEntries(
 	nonDefaultPermissions.map((p) => [p, z.coerce.boolean<boolean>().default(false)])
 ) as Record<NonDefaultPermission, z.ZodDefault<z.ZodBoolean>>;
 
-// 2. Combine into your final schema
 export const SetUserPermissions = z.object({
 	...permissionShape,
 	userId: z.string()
