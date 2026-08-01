6d7ea2e0c8e4463cc16b03791562e17a4740f2ad
Gurkan <hello@gurkz.me>
2026-08-01 16:15:43 +0200

    Revert "use drizzle pg-proxy"
    caused issues with dates :(
    
    might fix later
    
    This reverts commit 45a3d214d37ffb5565e2cbf5699b29b318aac7bd.

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,3 @@
-import type { DATABASE_URL } from "$app/env/private";
 import { defineEnvVars } from "@sveltejs/kit/env";
 import * as z from "zod/v4";
 
@@ -15,15 +14,6 @@ function skipInCI<T extends z.ZodTypeAny>(schema: T): T {
 export const variables = defineEnvVars({
 	DATABASE_URL: {
 		schema: skipInCI(z.url())
-	},
-	DATABASE_PROXY_URL: {
-		schema: skipInCI(z.url())
-	},
-	DATABASE_PROXY_NAME: {
-		schema: skipInCI(z.string())
-	},
-	DATABASE_PROXY_TOKEN: {
-		schema: skipInCI(z.string())
 	},
 	BETTER_AUTH_SECRET: {
 		schema: skipInCI(z.string())
