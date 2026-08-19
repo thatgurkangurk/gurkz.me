e2be951ceaca67ba530c6ae81029690c8d1f1d42
Gurkan <hello@gurkz.me>
2026-08-19 16:13:09 +0200

    i think this fixes it
    Signed-off-by: Gurkan <hello@gurkz.me>

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -54,7 +54,7 @@ export const auth = betterAuth({
 			clientId: env.DISCORD_CLIENT_ID,
 			clientSecret: env.DISCORD_CLIENT_SECRET,
 			prompt: "consent",
-			// overrideUserInfoOnSignIn: true,
+			overrideUserInfoOnSignIn: true,
 			mapProfileToUser: async (profile) => {
 				console.debug("profile", profile);
 				return {
@@ -81,7 +81,7 @@ export const auth = betterAuth({
 				type: "string",
 				unique: true,
 				required: true,
-				input: false
+				input: true
 			},
 			permissions: {
 				type: "string[]",
