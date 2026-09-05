040ad0f866c926be7a0d20c51933d9b96ad63aa4
Gurkan <hello@gurkz.me>
2026-09-05 18:52:31 +0200

    extract music card into its own component

--- src/routes/music.tsx
+++ src/routes/music.tsx
@@ -1,4 +1,5 @@
 import { orpc } from "#lib/orpc.js";
+import { MusicId } from "#lib/server/db/schema.js";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -12,24 +13,36 @@ export const Route = createFileRoute("/music")({
     },
 });
 
+function MusicCard(
+    props: Readonly<{
+        musicId: MusicId;
+    }>,
+) {
+    return (
+        <div
+            key={props.musicId.id}
+            style={{
+                border: "1px solid #ccc",
+                padding: "8px 12px",
+                borderRadius: "4px",
+            }}
+        >
+            <p style={{ margin: 0, fontWeight: "bold" }}>
+                {props.musicId.name}
+            </p>
+            <p style={{ margin: 0, color: "#666", fontSize: "14px" }}>
+                id: {props.musicId.robloxId}
+            </p>
+        </div>
+    );
+}
+
 function RouteComponent() {
     const { data } = useSuspenseQuery(orpc.music.list.queryOptions());
     return (
         <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
             {data.map((item) => (
-                <div
-                    key={item.id}
-                    style={{
-                        border: "1px solid #ccc",
-                        padding: "8px 12px",
-                        borderRadius: "4px",
-                    }}
-                >
-                    <p style={{ margin: 0, fontWeight: "bold" }}>{item.name}</p>
-                    <p style={{ margin: 0, color: "#666", fontSize: "14px" }}>
-                        id: {item.robloxId}
-                    </p>
-                </div>
+                <MusicCard key={item.id} musicId={item} />
             ))}
         </div>
     );
