9dd39a7fb84708492cce3e5a2676dc79890de6d1
Gurkan <hello@gurkz.me>
2026-09-06 13:58:50 +0200

    use the noop helper from tanstack query

--- src/routes/music/+page.ts
+++ src/routes/music/+page.ts
@@ -1,10 +1,12 @@
+import { noop } from "@tanstack/svelte-query";
+
 import type { PageLoad } from "./$types.js";
 import { musicIdsInfiniteQueryOptions } from "./query.js";
 
 export const load: PageLoad = async ({ parent, data }) => {
 	const { queryClient } = await parent();
 
-	await queryClient.infiniteQuery(musicIdsInfiniteQueryOptions()).catch(() => null);
+	await queryClient.infiniteQuery(musicIdsInfiniteQueryOptions()).catch(noop);
 
 	return data;
 };
