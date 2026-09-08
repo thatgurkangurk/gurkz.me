d4082a339dd2303889c1b8960efc8d8243bec3eb
Gurkan <hello@gurkz.me>
2026-09-08 10:33:19 +0200

    remove the (soon) label from delete button

--- src/lib/features/music/music-card.tsx
+++ src/lib/features/music/music-card.tsx
@@ -154,7 +154,7 @@ export function MusicCard({
                                 })
                             }
                         >
-                            delete (soon)
+                            delete
                         </Button>
                     </Check>
                 </div>
